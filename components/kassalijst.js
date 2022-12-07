94e6fa982a3107fbc089372b013c025730d063b5
Tom <syntraprogrammeurs@gmail.com>
2022-12-07 21:00:30 +0100

    tofixed toegevoegd

--- components/kassalijst.js
+++ components/kassalijst.js
@@ -5,13 +5,13 @@ app.component('kassalijst', {
              <div v-for="product in producten" :class="[product.purchased ? checkedClass : uncheckedClass]">
                  <p class="my-0 fs-3">{{product.article}}</p>
                  <div class="d-flex align-items-center">
-                    <p class="my-0 fs-3 pe-5">&euro; {{product.price}}</p>            
+                    <p class="my-0 fs-3 pe-5">&euro; {{product.price.toFixed(2)}}</p>            
                     <input @change="togglePurchased(product)" class="form-check-input fs-3 border border-danger rounded-circle m-0" type="checkbox" v-model="selectedProducts" :value="product">
                 </div>      
              </div>          
              <div class="shadow-lg p-3 mb-5 bg-dark rounded w-100 bg-opacity-75 border border-white border-5 d-flex justify-content-between">
                  <p class="my-0 fs-3">Total</p>
-                 <p class="my-0 fs-3">&euro; {{totalPrice}}</p>
+                 <p class="my-0 fs-3">&euro; {{totalPrice.toFixed(2)}}</p>
              </div>        
           </div>
     `,
@@ -20,13 +20,13 @@ app.component('kassalijst', {
             producten: [{
                 productId:1,
                 article:'Brood',
-                price: 3.00,
+                price: 3.20,
                 purchased: false,
             },
                 {
                     productId:2,
                     article:'Vlees',
-                    price: 5.00,
+                    price: 5.90,
                     purchased: false,
                 },
                 {
@@ -56,6 +56,7 @@ app.component('kassalijst', {
                 }
                 console.log(this.selectedProducts)
                 return total;
-        }
+        },
+
     }
 })
